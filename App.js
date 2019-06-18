2c9098cbd3389d7529d5526f6c05a45e5fb927ee
venkatainfotech <venkata.infotech@gmail.com>
2019-06-18 13:12:56 +0530

    2 files modified, 2 added

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header.js';
-
+import VunueNfo from './components/VunueNfo/index.js';
 
 function App() {
   return (
     <div className="App">      
           <Header /> 
+          
+          <VunueNfo/>
     </div>
   );
 }
