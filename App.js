15c67797ce6d80d49f5fac03a6c4e4860883782c
venkatainfotech <venkata.infotech@gmail.com>
2019-06-18 12:21:46 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './resources/styles.css';
+import Header from './components/header_footer/Header.js';
+
+
+function App() {
+  return (
+    <div className="App">      
+          <Header /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# 2venue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/2venue)
